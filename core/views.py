604faacba981bd6947486ee80b328016e1adb46c
JiniyasSuthr-2001 <jiniyassuthar87@gmail.com>
2026-01-21 14:44:55 +0530

    Fix missing JsonResponse import and local dependency issues

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.decorators.csrf import csrf_exempt
